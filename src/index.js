783af1578348fc4a935ad78798cb22433190fec5
blueyi1103 <32384717+blueyi1103@users.noreply.github.com>
2018-03-19 12:12:23 +0800

    0319-1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-window.addEventListener('load' , () =>{
-	let h1 = document.createElement('h1');
-	
-	h1.textContent = 'Hello World!' ;
-	
+'use strict';
+
+import Blog from './blog/blog.js';
+
+window.addEventListener('load' , () =>{   
+    
+	let bolg=Blog();
+    
+    document.getElementsByTagName('html')[0].style.height='100vh';
+    
+    document.body.style.height='100vh';
+    document.body.style.margin='0';
+    
 	document.body.appendChild(h1);
-{);
+});
